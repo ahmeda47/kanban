5ab36650c75c4eb137c4693c9d5bc2f352fdcd4a
ahmeda47 <abdiwahaba296@gmail.com>
2023-02-25 21:56:40 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value);
+  };
+  const addTodo = (value) => {
+    const newTodos = [...todos, value];
+    setTodos(newTodos);
+    console.log(todos);
+    setValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 20,
+          marginBottom: 20,
+          fontFamily: "fantasy",
+          fontSize: 25,
+        }}
+      >
+        <div
+          style={{
+            marginRight: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Create an item:
+        </div>
+        <input
+          style={{
+            marginRight: 5,
+          }}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button
+          style={{
+            backgroundColor: "lightgreen",
+            textAlign: "center",
+            textTransform: "uppercase",
+            letterSpacing: "1px",
+            border: "1px solid black",
+            borderRadius: "10px",
+          }}
+          onClick={handleSubmit}
+        >
+          Add
+        </button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            height: 800,
+            width: 500,
+            borderRight: "1px solid black",
+          }}
+        >
+          <p
+            style={{ textAlign: "center", fontFamily: "fantasy", fontSize: 25 }}
+          >
+            To do
+          </p>
+          <div>
+            {todos.map((todo) => {
+              return (
+                <Draggable>
+                  <div
+                    style={{
+                      margin: 10,
+                      width: 450,
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <div
+                      style={{
+                        border: "1px solid black",
+                        borderRadius: "25px",
+                        wordWrap: "break-word",
+                        width: 300,
+                        backgroundColor: "lightgreen",
+                      }}
+                    >
+                      <p style={{ textAlign: "center" }}>{todo}</p>
+                    </div>
+                  </div>
+                </Draggable>
+              );
+            })}
+          </div>
+        </div>
+        <div style={{ height: 800, width: 500, borderLeft: "1px solid black" }}>
+          <p
+            style={{ textAlign: "center", fontFamily: "fantasy", fontSize: 25 }}
+          >
+            Done
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
